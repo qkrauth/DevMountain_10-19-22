c95238d81cd3780147090ce3b2a1bcbc21e108bc
Quinten Krauth <quintenkrauth19@gmail.com>
2022-10-19 09:41:25 -0400

    warm up

---
+++ chal-roman-numerals/romanNumerals.js
@@ -0,0 +1,21 @@
+// Roman Numerals 
+
+// Difficulty:
+// Medium
+
+// Concepts:
+// General, Math
+
+// Write a function in romanNumerals.js that converts an integer (<= 1,000) to its Roman numeral equivalent.
+
+// For reference, these are the building blocks for how we encode numbers with Roman numerals:
+
+// For example:
+
+// toRoman(5)
+// 'V'
+// toRoman(267)
+// 'CCLXVII
+
+////////// CODE HERE //////////
+
